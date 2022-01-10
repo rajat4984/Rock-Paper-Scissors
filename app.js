98acbb510fe39fd3af4d4fb2337c92de25483665
Rajat <rs20021023@gmail.com>
2022-01-11 00:31:38 +0530

    getPlayerOption function done

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 let options = ["Rock","Paper","Scissors"]
 let randomOptionNum = Math.floor(Math.random() * 3)
+let playerOptionNum = 1
 
 
-getCompOption = (num) =>{
+const getPlayerOption = (num) =>{
+    return options[num]
+}
+
+
+const getCompOption = (num) =>{
     return options[num]
 } 
 
-let compOption = getCompOption(randomOptionNum)
+
+
+const compOption = getCompOption(randomOptionNum)
+const playerOption = getPlayerOption(playerOptionNum)
+
 console.log(compOption)
+console.log(playerOption)
 
