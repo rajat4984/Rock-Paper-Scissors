e37115d4c2275a3d707a034aea1af7f4f84fdde3
Rajat <rs20021023@gmail.com>
2022-01-11 01:21:19 +0530

    results function done

--- app.js
+++ app.js
@@ -1,22 +1,39 @@
-let options = ["Rock","Paper","Scissors"]
-let randomOptionNum = Math.floor(Math.random() * 3)
-let playerOptionNum = 1
+// ---------------Global variables-----------------------------
+
+let options = ["Rock", "Paper", "Scissors"];
+let compOptionNum = Math.floor(Math.random() * 3);
+let answer;
+
+let playerOption = prompt("Enter your option");
+let newPlayerOption = playerOption.toLocaleLowerCase();
+playerOption = newPlayerOption[0].toUpperCase() + newPlayerOption.substring(1);
+
+// ---------------Functions-----------------------------
 
 
-const getPlayerOption = (num) =>{
-    return options[num]
-}
+const getPlayerOption = (num) => {
+  return options[num];
+};
+
+const getCompOption = (num) => {
+  return options[num];
+};
+
+const results = (player, computer) => {
+  if (player === computer) {
+    return  "Its a draw";
+  } 
+  else if (player === "Rock" && computer === "Scissors" || player === "Paper" && computer === "Rock" || player === "Scissors" && computer === "Paper") {
+    return "Player wins";
+  }
+  else if(player === "Rock" && computer === "Paper" || player === "Paper" && computer === "Scissors" || player === "Scissors" && computer === "Rock"){
+      return "Computer wins";
+  }
+};
 
 
-const getCompOption = (num) =>{
-    return options[num]
-} 
-
-
-
-const compOption = getCompOption(randomOptionNum)
-const playerOption = getPlayerOption(playerOptionNum)
-
-console.log(compOption)
-console.log(playerOption)
+const compOption = getCompOption(compOptionNum);
+console.log(playerOption);
+console.log(compOption);
+console.log(results(playerOption, compOption));
 
