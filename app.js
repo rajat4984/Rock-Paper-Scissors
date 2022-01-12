d4f33d77c3a6eb7b3658ac121e4399d31859714d
Rajat <rs20021023@gmail.com>
2022-01-13 00:17:59 +0530

    now you can play whole game with UI

--- app.js
+++ app.js
@@ -8,7 +8,11 @@ let computerScore = Number(
 );
 let playerScore = Number(document.querySelector(".player-score").textContent);
 let buttons = document.querySelectorAll(".buttons");
-
+let popupButton = document.querySelector(".pop-up-button");
+let popupText = document.querySelector(".pop-up-text");
+let popup = document.querySelector("#pop-up");
+let winnerTextElement = document.querySelector(".winner-text");
+let winnerText;
 // ---------------Functions-----------------------------
 
 const getCompOption = (num) => {
@@ -17,41 +21,79 @@ const getCompOption = (num) => {
 
 const results = (player, computer) => {
   if (player === computer) {
-    return "Its a draw";
+    winnerTextElement.textContent = "Its a draw"
+    winnerTextElement.style.color = "yellow"
+    console.log("Its a draw");
   } else if (
     (player === "Rock" && computer === "Scissors") ||
     (player === "Paper" && computer === "Rock") ||
     (player === "Scissors" && computer === "Paper")
   ) {
     playerScore++;
+    winnerTextElement.textContent = `${player} beats ${computer} , well done `
+    winnerTextElement.style.color = "green"
     playerScoreElement.textContent = playerScore;
-    return "Player wins";
+    
+    console.log("player won");
   } else if (
     (player === "Rock" && computer === "Paper") ||
     (player === "Paper" && computer === "Scissors") ||
     (player === "Scissors" && computer === "Rock")
   ) {
     computerScore++;
+    winnerTextElement.textContent = `${computer} beats ${player} , oh no `
+    winnerTextElement.style.color = "red"
     computerScoreElement.textContent = computerScore;
-    return "Computer wins";
+    console.log("Computer won");
   }
+  checkWinner();
 };
 
 buttons.forEach((button) => {
   button.addEventListener("click", (e) => {
     let playerOption = e.target.className;
-    if (computerScore === 5 || playerScore === 5) {
-      e.disabled = true;
-    } else {
-      playGround(playerOption);
-    }
+    playGround(playerOption);
   });
 });
+
+const checkWinner = () => {
+  if (computerScore === 5) {
+    let winnerText = "Computer won the game";
+    willPlayAgain(winnerText,"red");
+  } else if (playerScore === 5) {
+    let winnerText = "You won the game";
+    willPlayAgain(winnerText,"green");
+  } else {
+    return true;
+  }
+};
 
 const playGround = (playerOption) => {
   let compOptionNum = Math.floor(Math.random() * 3);
   const compOption = getCompOption(compOptionNum);
   console.log(playerOption);
-  console.log(compOption);
-  console.log(results(playerOption, compOption));
+  if (checkWinner()) {
+    results(playerOption, compOption);
+  }
+};
+
+const willPlayAgain = (winnerText,color) => {
+  popupText.textContent = winnerText;
+  popupText.style.color = color
+  popup.style.display = "block";
+
+};
+
+popupButton.addEventListener("click", () => {
+  popup.style.display = "none";
+  resetGame();
+});
+
+const resetGame = () => {
+  playerScore = 0;
+  computerScore = 0;
+  playerScoreElement.textContent = playerScore;
+  computerScoreElement.textContent = computerScore;
+  winnerTextElement.textContent = "Lets start the game"
+  winnerTextElement.style.color = "white"
 };
